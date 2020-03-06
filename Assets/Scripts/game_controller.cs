8e3895e08653cbaccde6164fd47e859297304347
aeric90 <aeric90@gmail.com>
2020-03-05 20:21:43 -0500

    Main Game and UI Controllers Create
    Also added countdown functionality

---
+++ Assets/Scripts/game_controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_controller : MonoBehaviour
+{
+    public int gameTimer;
+
+    private bool gameOver = false;
+
+    void Start()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    private IEnumerator CountDown()
+    {
+
+        while(!gameOver)
+        {
+            if(gameTimer<=0)
+            {
+                gameOver = true;
+            }
+
+            yield return new WaitForSeconds(1.0f);
+            ui_controller.uiInstance.UpdateCountDown(TimerString());
+            gameTimer--;
+        }
+    }
+
+    private string TimerString()
+    {
+        int timerMinutes = gameTimer / 60;
+        int timerSeconds = gameTimer - (timerMinutes * 60);
+
+        return timerMinutes.ToString("00") + ":" + timerSeconds.ToString("00");
+    }
+
+}
---
+++ Assets/Scripts/ui_controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ui_controller : MonoBehaviour
+{
+    public static ui_controller uiInstance;
+
+    public TMPro.TextMeshProUGUI countDownText;
+    public TMPro.TextMeshProUGUI computerText;
+
+    void Start()
+    {
+        if(uiInstance == null) { uiInstance = this; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateCountDown(string timerString)
+    {
+        countDownText.text = timerString;
+    }
+}
