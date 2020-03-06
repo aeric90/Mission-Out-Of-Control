fee9025a73bf87e29f45e54f3191faef3bd3fb0b
aeric90 <aeric90@gmail.com>
2020-03-05 19:54:11 -0500

    Keypad Control is functional

---
+++ Assets/Scripts/keypad_controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keypad_controller : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI screenText;
+    private string keyPadValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClearScreen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenText.text = keyPadValue;
+    }
+
+    public void KeyClick(string keyName)
+    {
+        if(keyName == "clear")
+        {
+            ClearScreen();
+        }
+        else
+        {
+            if(keyPadValue.Length < 3)
+            {
+                keyPadValue += keyName;
+            }
+        }
+    }
+
+    private void ClearScreen()
+    {
+        keyPadValue = "";
+    }
+}
