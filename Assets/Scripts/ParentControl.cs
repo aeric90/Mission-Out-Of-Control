877acdc08e429e6f1815e67dd0a7e98b899775e9
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-03-05 20:36:08 -0500

    Slider

---
+++ Assets/Scripts/ParentControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentControl : MonoBehaviour
+{
+    public class Control
+    {
+        public int value;
+
+        public Control()
+        {
+            value = 0;
+        }
+
+        public int GetValue()
+        {
+            return value;
+        }
+    }
+}
---
+++ Assets/Scripts/SliderScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    public class Control
+    {
+        public int value;
+
+        public Control()
+        {
+            value = 0;
+        }
+
+        public int GetValue()
+        {
+            return value;
+        }
+    }
+
+    public class SliderControl : Control
+    {
+        public SliderControl()
+        {
+
+        }
+    }
+
+    SliderControl slider = new SliderControl();
+    public Text valueText;
+    public Slider sliderUI;
+
+    void Start()
+    {
+        valueText = transform.GetChild(0).gameObject.GetComponent<Text>();
+        sliderUI = transform.GetChild(1).gameObject.GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        slider.value = (int)sliderUI.value;
+        valueText.text = slider.value.ToString();
+    }
+}
