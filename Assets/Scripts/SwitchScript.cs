da26803c4ca5928644eae3bb61cf25a341a8de11
mohanpan <echoiepan@gmail.com>
2020-03-09 21:36:23 -0400

    SwitchPrefab
    Finished switch prefab.

---
+++ Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SwitchScript : MonoBehaviour
+{
+    public Button Switch;
+    public Sprite switchOn;
+    public Sprite switchOff;
+    private int counter = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Switch = GetComponent<Button>();
+    }
+
+    public void changeButton()
+    {
+        counter++;
+        if(counter % 2 == 1)
+        {
+            Switch.image.overrideSprite = switchOn;
+        }
+        else
+        {
+            Switch.image.overrideSprite = switchOff;
+        }
+    }
+    // Update is called once per frame
+}
