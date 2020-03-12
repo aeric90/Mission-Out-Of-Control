fc3870a201a6a1cf1ea8595b152f40598820e463
mohanpan <echoiepan@gmail.com>
2020-03-12 14:41:58 -0400

    switch control prefab done
    Switch control prefab done published to the main scene.

--- Assets/Scripts/switch_controller.cs
+++ Assets/Scripts/switch_controller.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static ParentControl;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class switch_controller : ParentControl
 {
+    public Image switchSprite;
+    public Sprite switchOn;
+    public Sprite switchOff;
+
     // Start is called before the first frame update
+    //0 off, 1 on.
     void Start()
     {
-        
+        value = "0";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnButtonPress()
     {
-        
+        if (value == "0")
+        {
+            switchSprite.overrideSprite = switchOn;
+            value = "1";
+        }
+        else
+        {
+            switchSprite.overrideSprite = switchOff;
+            value = "0";
+        }
     }
 }
