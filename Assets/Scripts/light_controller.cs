111b9351baa231e7636cd61c981cb6bf2764ef85
JohnathanRMB <60706026+JohnathanRMB@users.noreply.github.com>
2020-03-12 02:09:09 -0500

    Let There Be Light Update
    Made some more headway with the light script. Added buttons to panels

--- Assets/Scripts/light_controller.cs
+++ Assets/Scripts/light_controller.cs
@@ -1,19 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using static ParentControl;
 
 public class light_controller : ParentControl
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // string lightUI;
+
+    private void Start()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    // Is the light on or off?
+    public void LightOnOff()
     {
-        
+        // the control has been activated
+        if (this.GetActive())
+        {
+            this.GetValue();
+
+            // If the value string is 0, the light will shut off (turn grey)
+            if (this.value == "0")
+            {
+                Debug.Log(" value is 0, Lights Out!");
+                GetComponent<Image>().color = Color.grey;
+            }
+
+            // If the value string is 1, the light will turn on (turn white). 
+            else if (this.value == "1")
+            {
+                Debug.Log("value is 1 Lights On!");
+                GetComponent<Image>().color = Color.white;
+            }
+        }
     }
 }
