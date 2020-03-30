14bba517aeb469b0caa7ec0842879bc22bee94bb
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-03-30 12:08:13 -0400

    Added Menu and Transition Scenes

---
+++ Assets/Flicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Flicker : MonoBehaviour
+{
+    public GameObject redText;
+
+    private int counter;
+
+    void Start()
+    {
+        counter = 0;
+
+        StartCoroutine(FlashingText());
+    }
+
+    IEnumerator FlashingText()
+    {
+        while (true)
+        {
+            if (counter == 0)
+            {
+                redText.SetActive(true);
+                counter = 1;
+            }
+            else if(counter == 1)
+            {
+                redText.SetActive(false);
+                counter = 0;
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
+
+
