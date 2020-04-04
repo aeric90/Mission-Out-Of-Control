dd8c14e7d28199abb48a26c2c67af2c003b35862
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-04-04 00:08:14 -0400

    Sound

---
+++ Assets/Scripts/DoNotDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroy : MonoBehaviour
+{
+    private static DoNotDestroy _instance;
+    void Awake()
+    {
+        //if we don't have an [_instance] set yet
+        if (!_instance)
+            _instance = this;
+        //otherwise, if we do, kill this thing
+        else
+            Destroy(this.gameObject);
+
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
