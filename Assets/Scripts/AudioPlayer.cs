e6ec5f3cebe8550f896ad2e83817510a1d6e201b
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-04-05 15:43:27 -0400

    Planet Anims

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public static AudioPlayer audioPlayerInstance;
+
+    public AudioClip[] audioClips;
+    public AudioSource sourcePrefab;
+
+    private void Awake()
+    {
+        if (audioPlayerInstance == null) { audioPlayerInstance = this; }
+    }
+
+    public void PlayClip(AudioClip sound, bool loop, float volume)
+    {
+        AudioSource temp;
+
+        temp = Instantiate(sourcePrefab);
+
+        if (loop)
+        {
+            temp.loop = true;
+        }
+
+        temp.clip = sound;
+        temp.volume = volume;
+        temp.Play();
+    }
+}
---
+++ Assets/Scripts/MoveTowardsShip.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsShip : MonoBehaviour
+{
+    public static MoveTowardsShip moveTowardsShipInstance;
+
+    public Coroutine planet;
+    private bool isMoving = true;
+    private Vector3 end;
+
+    public void Awake()
+    {
+        if (moveTowardsShipInstance == null) { moveTowardsShipInstance = this; }
+        end = new Vector3(955.0f, 485.0f, 4000.0f);
+    }
+
+    public void StartPlanetMovement()
+    {
+        planet = StartCoroutine(MovePlanet());
+    }
+
+    IEnumerator MovePlanet()
+    {
+        float speed = 56.666666667f;
+
+        while (isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, end, Time.deltaTime * speed);
+
+            float diff = Mathf.Abs(transform.position.sqrMagnitude - end.sqrMagnitude);
+
+            if (diff < 5.0)
+            {
+                isMoving = false;
+                yield break;
+            }
+            yield return null;
+        }
+        StopCoroutine(planet);
+    }
+}
