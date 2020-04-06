2511d4c6c3e1ed99ca2e531dd8afc97c74e08d26
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-04-05 22:54:28 -0400

    Last SFX push

--- Assets/Scripts/TriggerGameStart.cs
+++ Assets/Scripts/TriggerGameStart.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class TriggerGameStart : MonoBehaviour
 {
     public GameObject overlayCanvas;
+    public GameObject sliderOne;
+    public GameObject sliderTwo;
 
     public void OnButtonPress()
     {
         game_controller.gameInstance.StartGame();
 
         overlayCanvas.SetActive(false);
+        sliderOne.GetComponent<AudioSource>().enabled = true;
+        sliderTwo.GetComponent<AudioSource>().enabled = true;
     }
 }
